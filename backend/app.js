9f2e9d1221802cc36d1d6d1eedaa8aecebbb6ce1
KNR1997 <kethakaranasinghe@gmail.com>
2026-03-29 02:14:19 +0530

    feat: Post crud

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import posts from "./routes/posts.js";
+const port = process.env.PORT || 8000;
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+import notFound from "./middleware/notFound.js";
+
+const app = express();
+
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Logger middleware
+app.use(logger);
+
+// Routes
+app.use("/api/posts", posts);
+
+// Error handler
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
