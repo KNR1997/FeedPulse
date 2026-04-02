de5a3612dc1b1eb18b10cf2e65c485614c4e6035
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-02 14:31:10 +0530

    feat: update Feedback and Login interfaces for consistency and clarity

--- frontend/types/index.ts
+++ frontend/types/index.ts
@@ -17,10 +17,28 @@ export interface Feedback {
   category: FeedbackCategoryType;
   status: FeedbackStatusType;
   description: string;
-  
   createdAt: string;
+}
+
+export interface CreateFeedbackInput {
+  title: string;
+  category: FeedbackCategoryType;
+  status: FeedbackStatusType;
+  description: string;
 }
 
 export interface UpdateFeedbackData {
   status: FeedbackStatusType;
+}
+
+export interface LoginInput {
+  email: string;
+  password: string;
+}
+
+export interface LoginReponse {
+  _id: string;
+  name: string;
+  email: string;
+  token: string;  
 }
