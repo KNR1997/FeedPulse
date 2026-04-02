b54ce54a8d22d35ba11cb492c93e08d0650c5de8
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-02 13:35:47 +0530

    feat: enhance feedback API with authorization token handling

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,7 @@
 import axios from "axios";
+import Cookies from "js-cookie";
+import { LoginInput } from "@/types";
+import { AUTH_CRED } from "@/utils/constants";
 
 export const getFeedbacks = async (filters?: {
   category?: string;
@@ -7,6 +10,7 @@ export const getFeedbacks = async (filters?: {
   limit?: number;
 }) => {
   const params = new URLSearchParams();
+  const token = Cookies.get(AUTH_CRED);
 
   if (filters?.category) params.append("category", filters.category);
   if (filters?.status) params.append("status", filters.status);
@@ -15,28 +19,59 @@ export const getFeedbacks = async (filters?: {
 
   const res = await axios.get(
     `http://localhost:8080/api/feedbacks?${params.toString()}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
   return res.data;
 };
 
 export const getFeedback = async (feedbackId: string) => {
+  const token = Cookies.get(AUTH_CRED);
+
   const res = await axios.get(
     `http://localhost:8080/api/feedbacks/${feedbackId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
   return res.data;
 };
 
 export const updateFeedback = async (id: string, data: { status: string }) => {
+  const token = Cookies.get(AUTH_CRED);
+
   const res = await axios.put(
     `http://localhost:8080/api/feedbacks/${id}`,
     data,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
   return res.data;
 };
 
 export const deleteFeedback = async (feedbackId: string) => {
+  const token = Cookies.get(AUTH_CRED);
+
   const res = await axios.delete(
     `http://localhost:8080/api/feedbacks/${feedbackId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
+  return res.data;
+};
+
+export const login = async (data: LoginInput) => {
+  const res = await axios.post(`http://localhost:8080/api/auth/login`, data);
   return res.data;
 };
