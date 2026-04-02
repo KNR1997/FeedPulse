69ae23041bac751c65bf6309f9ce39e46829eeb8
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-02 14:32:38 +0530

    feat: remove feedback page and associated FeedbackForm component

--- frontend/app/(public)/feedback/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// components
-import { FeedbackForm } from "@/components/feedback/feedback-form";
-
-const feedback = () => {
-  return <FeedbackForm />;
-};
-
-export default feedback;
