b7e7acae2c899c49f3e719657fac79eb256f12df
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-03 11:21:22 +0530

    feat(feedback-page): refactor feedback retrieval to use useFeedbackQuery hook

--- frontend/app/(app)/feedbacks/[id]/page.tsx
+++ frontend/app/(app)/feedbacks/[id]/page.tsx
@@ -1,30 +1,25 @@
 "use client";
 
 import { useParams } from "next/navigation";
-import { useQuery } from "@tanstack/react-query";
-// lib
-import { getFeedback } from "@/lib/api";
+// hooks
+import { useFeedbackQuery } from "@/data/feedback";
 // components
 import { FeedbackAdminForm } from "@/components/feedbacks/feedback-form";
 
 export default function FeedbackPage() {
-  const params = useParams(); // get route params
-  const feedbackId = params.id; // dynamic [id] from route
+  const params = useParams();
+  const feedbackId = params.id;
+  // query
+  const { feedback, loading, error } = useFeedbackQuery(feedbackId as string);
 
-  const { data, isLoading, isError } = useQuery({
-    queryKey: ["feedback", feedbackId],
-    queryFn: () => getFeedback(feedbackId as string),
-    enabled: !!feedbackId,
-  });
-
-  if (isLoading) return <p>Loading feedbacks...</p>;
-  if (isError) return <p>Failed to load feedbacks</p>;
+  if (loading) return <p>Loading feedbacks...</p>;
+  if (error) return <p>Failed to load feedbacks</p>;
 
   return (
     <div className="my-10 px-4 lg:px-6 max-w-[95rem] mx-auto w-full flex flex-col gap-4">
       <h3 className="text-xl font-semibold">View Feedback</h3>
 
-      <FeedbackAdminForm initialValues={data} />
+      <FeedbackAdminForm initialValues={feedback} />
     </div>
   );
 }
