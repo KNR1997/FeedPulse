51a62cf8f16540ba0539edd4312f4f2e99780dd2
KNR1997 <kethakaranasinghe@gmail.com>
2026-04-05 13:50:02 +0530

    feat: add Chip around mostCommonTag

--- frontend/components/home/card-balance3.tsx
+++ frontend/components/home/card-balance3.tsx
@@ -1,4 +1,4 @@
-import { Card, CardBody } from "@nextui-org/react";
+import { Card, CardBody, Chip } from "@nextui-org/react";
 import React from "react";
 import { Community } from "../icons/community";
 
@@ -10,7 +10,7 @@ export const CardBalance3 = ({mostCommonTag}: {mostCommonTag: string}) => {
           <Community />
           <div className="flex flex-col">
             <span className="text-white">Most Common Tag</span>
-            <span className="text-white text-xs">{mostCommonTag}</span>
+            <span className="text-white text-xs"><Chip>{mostCommonTag}</Chip></span>
           </div>
         </div>
         {/* <div className="flex gap-2.5 py-2 items-center">
